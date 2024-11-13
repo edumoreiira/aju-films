752c142cdcbc97275351f1438bd7e1cfccbba115
Eduardo <dudu_moreira9@hotmail.com>
2024-11-13 10:44:32 -0300

    feat(navbar): add blur fx on scroll

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  isScrolled = false;
+
+  ngOnInit(): void {
+    if(typeof window !== 'undefined') {
+      this.checkScroll();
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    this.checkScroll();
+  }
+
+  checkScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
 
 }
