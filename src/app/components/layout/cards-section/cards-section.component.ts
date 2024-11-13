22a1610369ad8bfacbf0343eb541ec397444bcca
Eduardo <dudu_moreira9@hotmail.com>
2024-11-13 11:19:49 -0300

    feat(home): add quality slogan text

---
+++ src/app/components/layout/cards-section/cards-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './cards-section.component.html',
+  styleUrl: './cards-section.component.scss'
+})
+export class CardsSectionComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardsSectionComponent } from "../../components/layout/cards-section/cards-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardsSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
