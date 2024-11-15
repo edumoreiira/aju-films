325029f6b0860613cc4232be0ebb0a22cfde8d71
Eduardo <dudu_moreira9@hotmail.com>
2024-11-15 17:31:04 -0300

    feat: add footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardsSectionComponent } from "../../components/layout/cards-section/cards-section.component";
 import { VideoCardComponent } from "../../components/layout/video-card/video-card.component";
+import { FooterComponent } from "../../components/layout/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardsSectionComponent, VideoCardComponent],
+  imports: [CardsSectionComponent, VideoCardComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
