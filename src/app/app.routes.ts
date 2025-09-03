910a5169ce6a6d1a52daebfe137328bd7e581d84
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-09-03 15:48:48 -0300

    fix(routes): ensure other routes redirect to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
